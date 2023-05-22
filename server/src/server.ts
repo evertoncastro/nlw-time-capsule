4584b7f3c513f776bd70ca2d1e9c36ed63cef3b3
Everton Castro <contato@evertoncastro.com>
2023-05-22 14:29:55 -0300

    Upload route

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,11 +3,21 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import jwt from '@fastify/jwt'
 import cors from '@fastify/cors'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
 const port = 3333
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
@@ -19,6 +29,7 @@ app.register(jwt, {
 
 app.register(authRoutes)
 app.register(memoriesRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
